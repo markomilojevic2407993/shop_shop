db8f0397d76c5aa20251e4459ac87d4deb72be54
markomilojevic2407993 <markomilojevic2407993@gmail.com>
2023-10-11 10:18:54 +0200

    search

--- modeli/search.php
+++ modeli/search.php
@@ -1,69 +1,41 @@
-
-
-
-
 <?php   
 
-$nav=[
-    'Home'=>'index.php',
-    'Registration'=>'registration.php',
-    'Login'=>'login.php',
-    'Get products'=>'getProduct.php',
-    'About'=>'about.php',
-    'Contact'=>'contact.php'
-];
+require_once "baza.php";
 
-require_once "modeli/baza.php";
+if(!isset($_GET['search'])||empty($_GET['search'])){
+    die('niste uneli podatke');
+}
 
-$rezultat=$baza->query("SELECT * FROM proizvodi");
+$search=$_GET['search'];
+
+$rezultat=$baza->query("SELECT * FROM proizvodi WHERE ime LIKE '%$search%' or opis LIKE '%$search%' ");
+
+
 $proizvodi=$rezultat->fetch_all(MYSQLI_ASSOC);
 
-//var_dump($proizvodi); exit();
+if ($proizvodi==false) {
+    echo 'Nothing found in the search.';
+}
+
 
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>shop</title>
+    <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../style.css">
 </head>
 <body>
+
+
+<div class="container">
     
 
-
-
-
-<nav class="navbar navbar-expand-lg navbar-dark bg-info">
-  <div class="container">
-    <a class="navbar-brand" href="#"></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-      <?php foreach($nav as $navKey=> $navValue):  ?>
-        <li class="nav-item">
-          <a class="nav-link active " aria-current="page" href="<?=$navValue?>"><?=$navKey?></a>
-        </li>
-        <?php endforeach;  ?>
-      </ul>
-    </div>
-  </div>
-</nav>
-<h1 class="text-center" ><?= $naslov?></h1>
-<div class="container" >
-<div class="row">
-  
-<form class="search" action="modeli/search.php" method="get">
-  <input type="text" name="search" placeholder="search">
-  <button type="submit">search</button>
-</form>
 
 <?php  foreach($proizvodi as $proizvod):  ?>
   <div class="col-md-4">
@@ -91,7 +63,6 @@ $proizvodi=$rezultat->fetch_all(MYSQLI_ASSOC);
   <?php endforeach; ?>
   </div>
 </div>
-
-
+</div>
 </body>
 </html>
